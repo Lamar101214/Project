84722b4ea9115c4a9c6e8ede21a410b0eb0ddd65
Lamar101214 <talabuconlaurenz@gmail.com>
2026-02-07 16:54:05 +0800

    Add files via upload

---
+++ TP.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ComputeAverageApp
+{
+    class ComputeAverageProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of values you want to average:");
+
+            double g1 = Convert.ToDouble(Console.ReadLine());
+            double g2 = Convert.ToDouble(Console.ReadLine());
+            double g3 = Convert.ToDouble(Console.ReadLine());
+            double g4 = Convert.ToDouble(Console.ReadLine());
+            double g5 = Convert.ToDouble(Console.ReadLine());
+
+            double average = (g1 + g2 + g3 + g4 + g5) / 5;
+
+            Console.WriteLine("The average is: " + average);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
